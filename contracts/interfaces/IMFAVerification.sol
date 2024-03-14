3d482f7aae09759e42d8ec83b2f808d3e55356ed
machin3boy <amir.kashapov@protonmail.ch>
2024-03-15 02:11:22 +1100

    feat: MFA interface

---
+++ contracts/interfaces/IMFAVerification.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+interface IMFAVerification {
+    struct MFAData {
+        bool success;
+        uint256 timestamp;
+    }
+
+    function setMFAData(uint256 requestId, bool success) external;
+
+    function setMFADataWithSignature(
+        uint256 requestId,
+        bool success,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+
+    function getMFAData(uint256 requestId)
+        external
+        view
+        returns (MFAData memory);
+}
