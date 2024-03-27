5ca083d4d54ce05c1dccfe4cff9d7d5e77e49e42
machin3boy <amir.kashapov@protonmail.ch>
2024-03-28 01:22:35 +1100

    chore: bigger button text

--- site/client/src/app/page.js
+++ site/client/src/app/page.js
@@ -45,11 +45,11 @@ function App() {
           <Logo className="h-10" />
           <div className="ml-2 font-bold text-2xl">zkVault</div>
         </div>
-        <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 font-semibold text-white text-lg inline-block mr-2">
+        <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px leading-6 font-bold text-white text-lg inline-block mr-2">
           <span className="absolute inset-0 overflow-hidden rounded-full">
             <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
           </span>
-          <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-1 px-6 ring-1 ring-white/10 bg-sky-800/20">
+          <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-1 px-6 ring-1 ring-white/10 bg-sky-800/30">
             <span>Connect Wallet</span>
             <svg
               fill="none"
