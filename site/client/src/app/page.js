ca193d5df20298c63a53602409db5e16f569cd31
machin3boy <amir.kashapov@protonmail.ch>
2024-03-28 01:27:17 +1100

    chore: connect wallet button background

--- site/client/src/app/page.js
+++ site/client/src/app/page.js
@@ -49,7 +49,7 @@ function App() {
           <span className="absolute inset-0 overflow-hidden rounded-full">
             <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
           </span>
-          <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-1 px-6 ring-1 ring-white/10 bg-sky-800/30">
+          <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-1 px-6 ring-1 ring-white/10 bg-sky-600/10">
             <span>Connect Wallet</span>
             <svg
               fill="none"
